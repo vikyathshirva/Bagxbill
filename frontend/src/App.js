0944a4a3251402985ddb2ffc4a82f6cf510bb43c
vikyathshirva <vicky.fames@gmail.com>
2021-02-14 17:45:12 +0530

    LinkContainer Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     <Container>
     
       <Route path='/' component={HomeScreen} exact />
-      <Route path='/product/:id' component={ProductScreen} exact />
+      <Route path='/product/:id' component={ProductScreen} />
     
     </Container>
     
