327587ef65f5bc9df838eac7e7f057a6d7a94e74
vikyathshirva <vicky.fames@gmail.com>
2021-02-17 21:22:41 +0530

    Error handling with json responses

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -31,7 +31,8 @@ import Product from '../models/productModel.js'
             res.json(product)
         }
         else{
-            res.status(404).json({message: 'Product not found'})
+            res.status(404)
+            throw new Error('Product not found')
         }
     }))
 
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors  from 'colors'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 
-app.get('/', (req,res) => {
-    res.send('API is running..')
-})
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes)
+app.use(notFound);
 
-const PORT = process.env.PORT || 5000
+app.use(errorHandler);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+);
