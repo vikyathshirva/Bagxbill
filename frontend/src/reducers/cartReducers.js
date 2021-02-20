bd3a104028f9f1153c1560c82e03d910531ccbc4
vikyathshirva <vicky.fames@gmail.com>
2021-02-20 12:17:18 +0530

    Order Summary implemented

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/CartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS,CART_SAVE_PAYMENT_METHOD} from '../constants/CartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
     switch (action.type) {
@@ -32,7 +32,13 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
       case CART_SAVE_SHIPPING_ADDRESS:
         return {
           ...state,
-          shippingAddress : action.payload,
+          shippingAddress: action.payload,
+        };
+
+      case CART_SAVE_PAYMENT_METHOD:
+        return {
+          ...state,
+          paymentMethod: action.payload,
         };
 
       default:
