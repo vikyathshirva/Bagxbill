6736658509ddde5ee80856228423f9c7d6ba76da
vikyathshirva <vicky.fames@gmail.com>
2021-02-20 13:35:07 +0530

    Order API implemented

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import CheckoutSteps from "../components/CheckoutSteps";
+import {createOrder} from '../actions/orderActions'
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({history}) => {
+
+  const dispatch = useDispatch()
+  
   const cart = useSelector((state) => state.cart);
 
   
@@ -24,9 +28,29 @@ const PlaceOrderScreen = () => {
     Number(cart.shippingPrice) +
     Number(cart.taxPrice)).toFixed(2)
 
+  const orderCreate = useSelector(state => state.orderCreate)
+  const { order, success,error} = orderCreate
+
+useEffect(() => {
+  if(success){
+    history.push(`/order/${order._id}`)
+  }else{
+
+  }
+  //eslint-disable-next-line
+}, [history, success ])
+
 
   const placeOrderHandler = () => {
-    console.log("order")
+    dispatch(createOrder({
+      orderItems: cart.cartItems,
+      shippingAddress: cart.shippingAddress,
+      paymentMethod: cart.paymentMethod,
+      itemsPrice : cart.itemsPrice,
+      shippingPrice : cart.shippingPrice,
+      taxPrice : cart.taxPrice,
+      totalPrice : cart.totalPrice,
+    }))
   };
 
 
@@ -121,6 +145,10 @@ const PlaceOrderScreen = () => {
                 </Row>
               </ListGroup.Item>
 
+              <ListGroup.Item>
+                    {error && <Message variant='danger'>{error}</Message>}
+              </ListGroup.Item>
+                      
               <ListGroup.Item>
                 <Button
                   type="button"
