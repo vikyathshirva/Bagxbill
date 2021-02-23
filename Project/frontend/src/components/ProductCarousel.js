ff0b77031c2654e9ffe54875ccebdd456dcf21cf
Vikyath Shirva <vicky.fames@gmail.com>
2021-02-24 03:02:59 +0530

    Prepare for deployment

--- Project/frontend/src/components/ProductCarousel.js
+++ Project/frontend/src/components/ProductCarousel.js
@@ -20,7 +20,7 @@ const ProductCarousel = () => {
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
         <Carousel pause='hover' className='bg-dark'>
             {products.map((product,index) => (
-                <Carousel.Item key='product._id)'>
+                <Carousel.Item key='{product._id}'>
                     <Link to={`/product/${product._id}`}>
                         <Image src={product.image} alt={product.name} fluid />
                         <Carousel.Caption className='carousel-caption'>
